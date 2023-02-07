ee1b72798a5756e9904c8b79fe6d92df191370b9
Dinesh Ram <dineshrameva908@gmail.com>
2023-02-07 12:55:10 +0530

    done

---
+++ init_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Task', 'Content for the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Task', 'Content for the second post')
+            )
+
+connection.commit()
+connection.close()
